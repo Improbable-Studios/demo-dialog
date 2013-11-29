e2d6362f1387a4c2304fda14c289cfd7efb8686d
Drennuz <xiayunsun@gmail.com>
2013-11-30 01:23:30 +0800

    major re-design: implemented line class and Speak function

--- Assets/talk.js
+++ Assets/talk.js
@@ -1,17 +1,68 @@
-﻿#pragma strict
+﻿/*
+mock dialog system for game Sherlock
+Xiayun Sun
 
-var talkGUI:GUIText;
-var lines:String[];
+TODO:
+1. add sound + typing effect.  
+2. change brightness of the texture? 
+3. line should always center
+*/
 
+#pragma strict
+
+var lineGUI:GUIText; 
+var charNameGUI: GUIText; 
+
+//GUITexture, displaying different texture while talking
+var leftChar:GUITexture; 
+var rightChar:GUITexture; 
+
+// create a line object
+class line {
+	var name : String; // character name
+	var text : String; // talk text
+	var charTextureGUI : GUITexture; // character display
+	var charTexture:Texture; // resource path of the GUITexture
+
+	function line(name : String, text : String, charTextureGUI:GUITexture, charTexture:Texture){
+		this.name = name;
+		this.text = text;
+		this.charTextureGUI = charTextureGUI;
+		this.charTexture = charTexture;
+	}
+}
+
+// load all textures, must be called in main thread...;
 private var currLine:int;
+var tex_JohnNeutral : Texture;
+var tex_JohnAnnoyed : Texture;
+var tex_SherlockNeutral : Texture;
+var tex_SherlockThinking : Texture;
 
 function Start () {
 	currLine = 0;
 }
 
-function Update () {
+// these are actual contents input
+var lines:line[];
+lines = new line[4];
+lines[0] = new line("John", "Sherlock, have you got a minute? ", leftChar, tex_JohnNeutral);
+lines[1] = new line("Sherlock", "I have several. They stretch before me in a never ending stream of tedium",rightChar, tex_SherlockNeutral);
+lines[2] = new line("John", "Dimmock just texted me about a case..",leftChar, tex_JohnAnnoyed);
+lines[3] = new line("Sherlock", "No",rightChar, tex_SherlockThinking);
+
+
+function Update () { 
 	if(Input.GetButtonDown("talk") && currLine < lines.Length){
-			talkGUI.text = lines[currLine]; 
+			Speak(lines[currLine]); 
 			currLine++;
 	}
+}
+
+function Speak(ln:line){
+	// main function for speak
+	charNameGUI.text = ln.name;
+	lineGUI.text = ln.text;
+	ln.charTextureGUI.texture = ln.charTexture;
+	
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,16 @@
-[Here](http://cgcookie.com/unity/2012/02/20/unity-script-npc-talk-system/) is a good tutorial on how to use Unity for a dialog system. 
+###Resources:
 
-Below are the barebone steps: 
+* [Here](http://cgcookie.com/unity/2012/02/20/unity-script-npc-talk-system/) is a good tutorial on how to use Unity for a dialog system. 
 
-* Create a new 2D project. 
-* For each talking character, create a GameObject. Bind a script to this object. The script is doing the talking. (`Assets/talk.js`)
-* The logic of dialog is very simple: 
-	* create a `GUIText` object for displaying the dialog and bind to the character object. 
-	* store all possible lines in an array (or can read from a file). You'll be able to modify the string array in Unity's GUI after binding the script to the character object. 
-	* in the `update` function in the script, proceed to the next line when a specific key is pressed down (through `Input.GetButtonDown()` function). To add a new key control, go to Edit --> Project Settings --> Input --> InputManager
+###Main
+
+Implemented a `line` class and `Speak` function that will: 
+
+1. display corresponding character name and dialog content; 
+2. change the character texture to allow for different poses
+
+###To do: 
+
+* typing + sound effect
+* change brightness of out-of-focus character
+* line should always center in the dialog box
